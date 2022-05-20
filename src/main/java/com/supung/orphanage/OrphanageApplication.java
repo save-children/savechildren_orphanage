91be521bb833eec7782268a988be1a1c4b8ba5c4
supun_g <supun.g@synergenhealth.com>
2022-05-20 11:45:52 +0530

    metrics

--- src/main/java/com/supung/orphanage/OrphanageApplication.java
+++ src/main/java/com/supung/orphanage/OrphanageApplication.java
@@ -1,9 +1,12 @@
 package com.supung.orphanage;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RefreshScope
@@ -12,6 +15,11 @@ public class OrphanageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrphanageApplication.class, args);
+	}
+
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 
 }
--- src/main/java/com/supung/orphanage/controller/OrphanageController.java
+++ src/main/java/com/supung/orphanage/controller/OrphanageController.java
@@ -6,6 +6,7 @@ import com.supung.orphanage.model.dto.OrphanageOutputDTO;
 import com.supung.orphanage.service.OrphanageService;
 import com.supung.orphanage.service.OrphanageServiceMapper;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.micrometer.core.annotation.Timed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,32 +29,38 @@ public class OrphanageController {
     }
 
     @PostMapping
+    @Timed(value = "add.time", description = "Time taken to add an orphanage")
     public ResponseEntity<OrphanageOutputDTO> add(@RequestBody OrphanageInputDTO orphanageInputDTO) {
         return ResponseEntity.ok(orphanageService.save(orphanageInputDTO));
     }
 
     @GetMapping
     @RateLimiter(name = "getAll", fallbackMethod = "getAllFallback")
+    @Timed(value = "getAll.time", description = "Time taken to get all orphanages")
     public ResponseEntity<List<OrphanageOutputDTO>> getAll(@RequestParam(required = false) Boolean isDeleted) {
         return ResponseEntity.ok(orphanageService.findAll(isDeleted));
     }
 
     @GetMapping("/{id}")
+    @Timed(value = "getById.time", description = "Time taken to get an orphanage by id")
     public ResponseEntity<OrphanageOutputDTO> getById(@PathVariable long id) {
         return ResponseEntity.ok(orphanageService.findById(id));
     }
 
     @PutMapping("/{id}")
+    @Timed(value = "update.time", description = "Time taken to update an orphanage by id")
     public ResponseEntity<OrphanageOutputDTO> update(@PathVariable long id, @RequestBody OrphanageInputDTO orphanageInputDTO) {
         return ResponseEntity.ok(orphanageService.update(id, orphanageInputDTO));
     }
 
     @DeleteMapping("/{id}")
+    @Timed(value = "delete.time", description = "Time taken to delete an orphanage by id")
     public OrphanageOutputDTO delete(@PathVariable long id) {
         return orphanageService.inactivate(id);
     }
 
     @GetMapping("/{id}/getWithRequirements")
+    @Timed(value = "getWithRequirements.time", description = "Time taken to get an orphanage with requirements")
     public ResponseEntity<CompactOrphanageWrapperDTO> getWithRequirements(@RequestHeader("correlation-id") String correlationId,
                                                                           @PathVariable long id) {
         log.info("Finding orphanage with requirements by orphanage id : {}, correlationId : {}", id, correlationId);
